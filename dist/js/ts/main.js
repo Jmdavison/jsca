2635c9c1019b22de98c8702ec956b8bfccf6040d
Jacob Davison <jmdavison@wccnet.edu>
2017-10-25 16:44:28 -0400

    and that

--- dist/js/ts/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var button = document.querySelector("restartButton");
-    var jsca = new JSCA.J2D(30);
-    button.addEventListener("pointerdown", function () {
-        jsca.play();
-    });
-});
